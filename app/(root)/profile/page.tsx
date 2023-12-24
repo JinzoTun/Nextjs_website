cf01f21302c62cbfc03069f92de5eb803bb6d237
Koussay Hajri <hajrikoussay03@gmail.com>
2023-12-24 22:42:36 +0100

    Update page.tsx

--- app/(root)/profile/page.tsx
+++ app/(root)/profile/page.tsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const myProfile = () => {
   return (
-    <div>myProfile</div>
+    <div>myProfile
+      <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4859791566235198"
+     crossOrigin="anonymous"></script>
+    </div>
   )
 }
 
