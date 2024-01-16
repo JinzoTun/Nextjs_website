548a27d5e3b6daa48f4507d0b66561f721d8c810
Koussay Hajri <hajrikoussay03@gmail.com>
2024-01-16 17:45:40 +0100

    Update ResourceCard.tsx

--- components/ResourceCard.tsx
+++ components/ResourceCard.tsx
@@ -13,7 +13,7 @@ interface Props {
 
 const ResourceCard = ({ id, title, image, downloadNumber, downloadLink }: Props) => {
   return (
-    <Card className="w-full max-w-fit border-0 !bg-transparent sm:max-w-[356px]">
+    <Card className="w-full max-w-fit border-0 !bg-transparent ">
       <Link href={downloadLink} target="_blank">
         <CardHeader className="flex-center flex-col gap-2.5 !p-0">
           <div className="h-fit w-full">
