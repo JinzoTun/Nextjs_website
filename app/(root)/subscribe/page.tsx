0973f7c7f778cc55263d5c8843efdcd60872aa75
Koussay Hajri <hajrikoussay03@gmail.com>
2024-01-16 17:18:50 +0100

    5

--- app/(root)/subscribe/page.tsx
+++ app/(root)/subscribe/page.tsx
@@ -1,8 +1,13 @@
+import Navbar from '@/components/Navbar'
+import { Button } from '@/components/ui/button'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>subscribe</div>
+    <>
+    </>
+
+
   )
 }
 
