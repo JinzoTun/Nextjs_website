62ed3e453324a24af9a467258419b7e738ca140b
Koussay Hajri <hajrikoussay03@gmail.com>
2024-01-16 17:38:37 +0100

    Update ResourceCard.tsx

--- components/ResourceCard.tsx
+++ components/ResourceCard.tsx
@@ -20,8 +20,8 @@ const ResourceCard = ({ id, title, image, downloadNumber, downloadLink }: Props)
             <Image 
               src={image}
               className="h-full rounded-md object-cover"
-              width={384}
-              height={440}
+              width={192}
+              height={220}
               alt={title}
             />
           </div>
@@ -31,13 +31,13 @@ const ResourceCard = ({ id, title, image, downloadNumber, downloadLink }: Props)
       <CardContent className="flex-between mt-4 p-0">
         <div className="flex-center body-medium gap-1.5 text-white">
           <Image 
-            src="/downloads.svg" width={20} height={20} alt="download"
+            src="/downloads.svg" width={15} height={15} alt="download"
           />
           {downloadNumber}
         </div>
         <Link href={downloadLink} target="_blank" className="flex-center text-gradient_purple-blue body-semibold gap-1.5">
           Download Now
-          <Image src="/arrow-blue.svg" width={13} height={10} alt="arrow" />
+          <Image src="/arrow-blue.svg" width={10} height={7} alt="arrow" />
         </Link>
       </CardContent>
     </Card>
